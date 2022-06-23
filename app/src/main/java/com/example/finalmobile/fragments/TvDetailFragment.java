45203fe404cf1f658972b6e212059627a61e8363
huypro020801 <bloghuy2818@gmail.com>
2022-06-23 11:09:08 +0700

    update some function

--- app/src/main/java/com/example/finalmobile/fragments/TvDetailFragment.java
+++ app/src/main/java/com/example/finalmobile/fragments/TvDetailFragment.java
@@ -22,21 +22,276 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.finalmobile.databinding.FragmentTvDetailBinding;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerControlView;
+import com.google.android.exoplayer2.ui.StyledPlayerControlView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.example.finalmobile.R;
+import com.example.finalmobile.activities.MainActivity;
+import com.example.finalmobile.asynctasks.ExecuteQueryAsync;
+import com.example.finalmobile.databinding.FragmentTvDeltailBinding;
+import com.example.finalmobile.listeners.CheckFavListener;
+import com.example.finalmobile.listeners.ExecuteQueryAsyncListener;
+import com.example.finalmobile.listeners.OnHomeItemClickListeners;
+import com.example.finalmobile.listeners.SetFavListener;
+import com.example.finalmobile.utils.Methods;
+import com.squareup.picasso.Picasso;
 
 import okhttp3.RequestBody;
 
 public class TvDetailFragment extends Fragment {
 
-    private FragmentTvDetailBinding binding;
+    private FragmentTvDeltailBinding binding;
+    private NavController navController;
+    private ExoPlayer player;
+    private Boolean mIsFav = false;
+    private int id;
+    Bundle bundle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentTvDetailBinding.inflate(inflater, container, false);
+        binding = FragmentTvDeltailBinding.inflate(inflater, container, false);
         View rootView = binding.getRoot();
+        bundle = getArguments();
+        id = bundle.getInt("id");
+        setUp();
+        Methods.getInstance().checkVideoFav(getContext(), id, new CheckFavListener() {
+            @Override
+            public void onComplete(boolean isSuccess, boolean isFav) {
+                if(isSuccess){
+                    mIsFav = isFav;
+                }
+                setUpFav();
+            }
+        });
 
         return rootView;
+    }
+
+    private void setUp(){
+        MainActivity.hide_Navi();
+        (getActivity()).setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        navController = NavHostFragment.findNavController(this);
+
+        binding.layoutMainDetailFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int state = -1;
+                if(binding.layoutInfTvDetailFrag.getVisibility() == View.VISIBLE){
+                    state = View.GONE;
+                    binding.controlViewTVDetailFrag.hide();
+                } else {
+                    state = View.VISIBLE;
+                    binding.controlViewTVDetailFrag.show();
+                }
+                binding.layoutInfTvDetailFrag.setVisibility(state);
+                binding.btnOutTvDetailFrag.setVisibility(state);
+            }
+        });
+
+
+        Picasso.get().load(bundle.getString("url_img")).into(binding.imgTvDetailFrag);
+        binding.txtTvDetailFrag.setText(bundle.getString("des"));
+
+        binding.txtTvDetailFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.controlViewTVDetailFrag.show();
+            }
+        });
+
+        binding.btnOutTvDetailFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bundle.getBoolean("isHome")){
+                    navController.navigate(R.id.TVDetail_to_HomeFrag);
+                } else if(bundle.getBoolean("isFavorite")) {
+                    navController.navigate(R.id.tv_detail_to_favorite);
+                }
+                else{
+                    navController.navigate(R.id.fromDetailtoTVFrag);
+                }
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.scrollViewTvDetailFrag.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+                @Override
+                public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                    binding.controlViewTVDetailFrag.show();
+                }
+            });
+        }
+
+        binding.layoutInfTvDetailFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.controlViewTVDetailFrag.show();
+            }
+        });
+
+        player = new ExoPlayer.Builder(getContext()).build();
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onPlaybackStateChanged(int playbackState) {
+                Player.Listener.super.onPlaybackStateChanged(playbackState);
+                switch (playbackState) {
+                    case Player.STATE_BUFFERING:
+                        binding.prgTvDetailFrag.setVisibility(View.VISIBLE);
+                        binding.imgTempTVDetailFrag.setVisibility(View.VISIBLE);
+                        break;
+                    case Player.STATE_READY:
+                        binding.prgTvDetailFrag.setVisibility(View.GONE);
+                        binding.imgTempTVDetailFrag.setVisibility(View.GONE);
+                        break;
+                    case Player.STATE_ENDED:
+                        break;
+                }
+            }
+        });
+        binding.videoTvDetailFrag.setUseController(false);
+        binding.videoTvDetailFrag.setPlayer(player);
+
+        binding.controlViewTVDetailFrag.addVisibilityListener(new PlayerControlView.VisibilityListener() {
+            @Override
+            public void onVisibilityChange(int visibility) {
+                if(visibility == View.VISIBLE){
+                    binding.layoutInfTvDetailFrag.setVisibility(View.VISIBLE);
+                    binding.btnOutTvDetailFrag.setVisibility(View.VISIBLE);
+                } else {
+                    binding.layoutInfTvDetailFrag.setVisibility(View.GONE);
+                    binding.btnOutTvDetailFrag.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        binding.controlViewTVDetailFrag.setPlayer(player);
+        binding.controlViewTVDetailFrag.setShowNextButton(false);
+        binding.controlViewTVDetailFrag.setShowPreviousButton(false);
+
+        MediaItem mediaItem = MediaItem.fromUri(bundle.getString("url"));
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();
+
+        binding.imgLikeTvdetailFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Methods.getInstance().setFavState(getContext(), id, !mIsFav, new SetFavListener() {
+                    @Override
+                    public void onComplete(boolean isSuccess) {
+                        if(isSuccess){
+                            mIsFav = !mIsFav;
+                            if(mIsFav){
+                                Picasso.get()
+                                        .load(R.drawable.ic_heart4_check)
+                                        .into(binding.imgLikeTvdetailFrag);
+                            }else{
+                                Picasso.get()
+                                        .load(R.drawable.ic_heart4_uncheckpng)
+                                        .into(binding.imgLikeTvdetailFrag);
+                            }
+                        }
+                    }
+                });
+                binding.controlViewTVDetailFrag.show();
+            }
+        });
+
+        binding.imgreport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showReportDialog();
+            }
+        });
+    }
+
+    private void showReportDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        View view1 = LayoutInflater.from(getContext()).inflate(R.layout.dialog_report_layout, null,false);
+        builder.setView(view1);
+        builder.setCancelable(false);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        ConstraintLayout cs_main = view1.findViewById(R.id.cs_main);
+        Button btn_submit = view1.findViewById(R.id.btn_submit);
+        Button btn_cancel = view1.findViewById(R.id.btn_cancel);
+        EditText edt_report_content = view1.findViewById(R.id.edt_report_content);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        try{
+            btn_submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String content = edt_report_content.getText().toString();
+                    ExecuteQueryAsyncListener listener = new ExecuteQueryAsyncListener() {
+                        @Override
+                        public void onStart() {}
+
+                        @Override
+                        public void onEnd(boolean status) {
+                            if(status){
+                            }else{
+                                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    };
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("uid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    bundle.putString("report_content",content);
+                    bundle.putInt("vid_id", id);
+
+                    RequestBody requestBody = Methods.getInstance().getReportRequestBody("INSERT_REPORT",bundle);
+                    ExecuteQueryAsync async = new ExecuteQueryAsync(requestBody, listener);
+                    async.execute();
+                    alertDialog.dismiss();
+                }
+            });
+
+        }
+        catch (Exception e){
+            Toast.makeText(getContext(), "Please input your report!", Toast.LENGTH_SHORT).show();
+        }
+
+        alertDialog.show();
+
+    }
+
+    private void setUpFav(){
+        if(mIsFav){
+            Picasso.get()
+                    .load(R.drawable.ic_heart4_check)
+                    .into(binding.imgLikeTvdetailFrag);
+        }else{
+            Picasso.get()
+                    .load(R.drawable.ic_heart4_uncheckpng)
+                    .into(binding.imgLikeTvdetailFrag);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if(player!=null){
+            player.stop();
+        }
+        MainActivity.show_Navi();
+        getActivity().setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
--- app/src/main/java/com/example/finalmobile/fragments/VideoFavoriteFragment.java
+++ app/src/main/java/com/example/finalmobile/fragments/VideoFavoriteFragment.java
@@ -15,8 +15,16 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.example.finalmobile.R;
+import com.example.finalmobile.adapters.FeaturedVideoAdapter;
+import com.example.finalmobile.asynctasks.LoadFavoriteListAsync;
 import com.example.finalmobile.databinding.FragmentVideoFavoriteBinding;
+import com.example.finalmobile.listeners.LoadSearchVideoAsyncListener;
+import com.example.finalmobile.listeners.OnVideoFeatureClickListener;
+import com.example.finalmobile.listeners.FavoriteToDetailListener;
+import com.example.finalmobile.models.Videos_M;
+import com.example.finalmobile.utils.Methods;
 
 import java.util.ArrayList;
 
@@ -25,6 +33,10 @@ import okhttp3.RequestBody;
 public class VideoFavoriteFragment extends Fragment {
     private View rootView;
     private FragmentVideoFavoriteBinding binding;
+    private ArrayList<Videos_M> arrayList_fav, mArrayList;
+    private FeaturedVideoAdapter videoAdapter;
+    private NavController navController;
+    private FavoriteToDetailListener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +44,112 @@ public class VideoFavoriteFragment extends Fragment {
         binding = FragmentVideoFavoriteBinding.inflate(inflater, container, false);
         rootView = binding.getRoot();
 
+        arrayList_fav = new ArrayList<Videos_M>();
+        navController = NavHostFragment.findNavController(this);
+
+        LoadData();
+
         return rootView;
     }
 
+    public VideoFavoriteFragment() {
+    }
+
+    public VideoFavoriteFragment(FavoriteToDetailListener listener) {
+        this.listener = listener;
+    }
+
+    private void LoadData(){
+        Bundle bundle = new Bundle();
+        bundle.putInt("vid_type",1);
+        bundle.putString("uid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+        RequestBody requestBody = Methods.getInstance().getVideoRequestBody("GET_FAV_DATA", bundle);
+        LoadSearchVideoAsyncListener listener = new LoadSearchVideoAsyncListener() {
+            @Override
+            public void onStart() {
+            }
+
+            @Override
+            public void onEnd(boolean status, ArrayList<Videos_M> arrayList) {
+                if(getContext() != null){
+                    if(Methods.getInstance().isNetworkConnected(getContext())){
+                        if(status){
+                            binding.progress.setVisibility(View.GONE);
+                            if(arrayList==null){
+                                binding.tvNoResult.setVisibility(View.VISIBLE);
+                                binding.recyclerFavVideo.setVisibility(View.GONE);
+                            }
+                            else{
+
+                                arrayList_fav.addAll(arrayList);
+                                mArrayList = arrayList_fav;
+                                updateUI();
+                            }
+
+                        }else{
+                            Toast.makeText(getContext(), "Something wrong happened, try again!", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getContext(), "Please connect to the internet!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        };
+        LoadFavoriteListAsync async = new LoadFavoriteListAsync(requestBody, listener, Methods.getInstance());
+        async.execute();
+
+    }
+
+    public void searchVideo(String text){
+        if (arrayList_fav!=null){
+            if(text.length()!=0){
+                ArrayList<Videos_M> list_search = new ArrayList<>();
+                for(Videos_M i : arrayList_fav){
+                    if(i.getVid_title().toLowerCase().contains(text.toLowerCase()))
+                        list_search.add(i);
+                }
+                if(list_search.isEmpty()) {
+                    if (text.length() > 0){}
+                    //Toast.makeText(getActivity(), "No TV Found", Toast.LENGTH_SHORT).show();
+                } else {
+                    videoAdapter.setList_Video(list_search);
+                    videoAdapter.notifyDataSetChanged();
+                }
+            }
+            else{
+                videoAdapter.setList_Video(mArrayList);
+                videoAdapter.notifyDataSetChanged();
+                //LoadData();
+
+            }
+        }
+        else{
+
+        }
+
+    }
+
+    private void updateUI() {
+        binding.recyclerFavVideo.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+
+        int width = getContext().getResources().getDisplayMetrics().widthPixels;
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) Math.round(width/2*0.8));
+        layoutParams.setMargins(10,20,10,20);
+
+        videoAdapter = new FeaturedVideoAdapter(Methods.getInstance(), layoutParams, arrayList_fav, new OnVideoFeatureClickListener() {
+            @Override
+            public void onClick(int position) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("video", arrayList_fav.get(position));
+                bundle.putBoolean("is_favorite", true);
+                listener.onDirect(1, bundle);
+
+            }
+        });
+
+        binding.recyclerFavVideo.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        binding.recyclerFavVideo.setAdapter(videoAdapter);
+
+    }
 }
